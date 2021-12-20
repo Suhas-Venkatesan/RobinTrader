e9a27cb1d7076ba2081b282eaf90e6959fbe1c4d
Suhas Venkatesan <vsuhas1@gmail.com>
2021-12-20 00:47:09 -0500

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,7 @@
+# Enter your robinhood username and password here, then save this file in the same directory as a regular python file
+rh_password = "test"
+rh_username = "test"
+# change to false to run
+debug = True
+# set to show plot; if set to false plot will not show
+plot = True
